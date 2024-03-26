e3310f081bb564156d8670a3ba1b36386e1da794
“Patricio <patojeder@gmail.com>
2024-03-26 16:51:00 -0300

    hice analisis de punteros en cpp

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -2,4 +2,13 @@
 
 int main(){
     printf("hola mundo");
+    int *puntero;
+    int numero=5;
+    puntero= &numero;
+    int tamanio=sizeof(int);
+    printf("El contenido del puntero %d\n", *puntero );
+    printf(" La dirección de memoria almacenada por el puntero %p\n", &numero);
+    printf("la dirección de memoria de la variable %p\n", &numero);
+    printf("la dirección de memoria del puntero. %p\n", &puntero);
+    printf("%d", tamanio);
 }
